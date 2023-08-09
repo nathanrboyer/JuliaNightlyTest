bccab7a0ae508461a979d207e6538721e22c857a
nathanrboyer <nathanrobertboyer@gmail.com>
2023-08-09 10:32:29 -0400

    Simplify reading

--- test.jl
+++ test.jl
@@ -5,10 +5,8 @@ using AlgebraOfGraphics, BenchmarkTools, CairoMakie, CSV, DataFrames, Polynomial
 includet("definitions.jl")
 
 # Read Data
-op_file = "OperatingStress.csv"
-res_file = "ResidualStress.csv"
-(s_op, σ_op, s_op_units, σ_op_units) = read_fracture_stress(op_file)
-(s_res, σ_res, s_res_units, σ_res_units) = read_fracture_stress(res_file)
+(s_op, σ_op, s_op_units, σ_op_units) = read_fracture_stress("OperatingStress.csv")
+(s_res, σ_res, s_res_units, σ_res_units) = read_fracture_stress("ResidualStress.csv")
 
 # Compute Polynomial Fits Individually
 # (This method still works okay on 1.10.0-beta1.)
@@ -20,5 +18,13 @@ res_results = polyfit(s_res, σ_res)
 op_results, res_results = polyfit([s_op, s_res], [σ_op, σ_res])
 
 # Plot Results
-op_plot = plotpolyfit(op_results; x_units=s_op_units, y_units=σ_op_units)
-res_plot = plotpolyfit(res_results; x_units=s_res_units, y_units=σ_res_units)
+op_plot = plotpolyfit(
+    op_results;
+    x_units = s_op_units,
+    y_units = σ_op_units,
+)
+res_plot = plotpolyfit(
+    res_results;
+    x_units = s_res_units,
+    y_units = σ_res_units,
+)
